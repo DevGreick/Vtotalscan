5aaf8c634d128e21f4cc720560b0944f458d0ad7
SecZeror <39761156+DevGreick@users.noreply.github.com>
2025-08-20 11:24:44 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -8,7 +8,6 @@ import os
 def resource_path(relative_path):
     """ Obtém o caminho absoluto para o recurso, funciona para dev e para o executável PyInstaller """
     try:
-        # PyInstaller cria uma pasta temporária e armazena o caminho em _MEIPASS
         base_path = sys._MEIPASS
     except Exception:
         base_path = os.path.abspath(".")
